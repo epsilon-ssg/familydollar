b8169a6f555b5989ebeada3c6e047b8c817e6e35
CORP\dveeraswamy <divya.veeraswamy@epsilon.com>
2017-05-11 16:47:52 +0530

    Component fixes

--- core/src/main/java/com/familydollar/corporate/core/use/FooterWCMUse.java
+++ core/src/main/java/com/familydollar/corporate/core/use/FooterWCMUse.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.adobe.cq.sightly.WCMUse;
 import com.familydollar.corporate.core.models.SocialIcon;
@@ -12,6 +17,8 @@ import com.google.gson.Gson;
 
 public class FooterWCMUse extends WCMUse{
 	
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+	
 	private String termsAndPrivacyUrl;
 	
 	private List<SocialIcon> socialIcons = new ArrayList<SocialIcon>();
@@ -19,6 +26,7 @@ public class FooterWCMUse extends WCMUse{
 	@Override
 	public void activate() throws Exception {
 		Node currentNode = getResource().adaptTo(Node.class);
+		addFooterLinkNodes(currentNode);
 		if(null != currentNode){
 			termsAndPrivacyUrl = currentNode.hasProperty("termsAndPrivacyUrl") ? currentNode.getProperty("termsAndPrivacyUrl").getString() : null;
 			Gson gson = new Gson();
@@ -36,6 +44,25 @@ public class FooterWCMUse extends WCMUse{
 		}
 	}
 	
+	/**
+	 * Temporary fix for AEM 6.0 issue when component is included
+	 * @param currentNode
+	 */
+	private void addFooterLinkNodes(Node currentNode) {
+		try {
+			if(!currentNode.hasNodes()) {
+				currentNode.addNode("footerLinks1");
+				currentNode.addNode("footerLinks2");
+				currentNode.addNode("footerLinks3");
+				Session session = getResourceResolver().adaptTo(Session.class);
+				session.save();
+			}
+		} catch (RepositoryException e) {
+			logger.error("RepositoryException",e); 
+		}
+		
+	}
+
 	private void getSocialIcon(Gson gson, String icon) {
 		SocialIcon socialIcon = gson.fromJson(icon, SocialIcon.class);
 		socialIcon.setIconLinkURL(LinksUtil.checkInternalURLByPath(socialIcon.getIconLinkURL(), getResourceResolver()));
